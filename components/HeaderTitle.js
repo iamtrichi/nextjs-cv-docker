3a7172f6b5296ad1e4b1e8c4160fa181f486db9f
iamtrichi <57832952+iamtrichi@users.noreply.github.com>
2020-12-21 12:24:38 +0100

    initial commit

--- components/HeaderTitle.js
+++ components/HeaderTitle.js
@@ -8,7 +8,7 @@ const cursor = {
 export default ({color}) => (
     <div className="is-hidden-touch">
         <Typist cursor={cursor}>
-            I'm <span>Nut</span>tawut
+            I'm <span>Tri</span>chi
         </Typist>
         <style jsx>{`
             div {
